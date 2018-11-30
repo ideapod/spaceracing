0a5d4b64cb8c561d2ddd275f64d4e06ea1dc1011
Mark Smith <ligalog@icloud.com>
2018-11-30 20:57:33 +1100

    chase camera controller

--- Assets/ChaseCameraController.cs
+++ Assets/ChaseCameraController.cs
@@ -12,15 +12,17 @@ public class ChaseCameraController : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        //Calculate and store the offset value by getting the distance 
+        // between the player's position and camera's position.
         offset = transform.position - player.transform.position;
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        // Set the position of the camera's transform to be the same as the player's, 
+        // but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset + offset/2;
     }
 }
 
